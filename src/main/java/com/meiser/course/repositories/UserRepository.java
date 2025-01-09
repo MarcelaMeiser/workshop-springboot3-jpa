1f1f93ebe76add6c85c7dc49a73fa290a2d468a0
Marcela Meiser <marcela.meiser@gmail.com>
2025-01-09 12:56:56 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/meiser/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.meiser.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.meiser.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+
+}
