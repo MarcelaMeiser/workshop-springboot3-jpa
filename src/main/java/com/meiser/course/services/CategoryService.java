56358d447b645bc35a48f7faadac0b09b71ffca0
Marcela Meiser <marcela.meiser@gmail.com>
2025-01-13 12:49:11 -0300

    Category

---
+++ src/main/java/com/meiser/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.meiser.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.meiser.course.entities.Category;
+import com.meiser.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
