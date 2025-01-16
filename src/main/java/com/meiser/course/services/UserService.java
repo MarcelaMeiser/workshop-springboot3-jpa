e81823541d43b79c0f81e418590f906b61da5cc3
Marcela Meiser <marcela.meiser@gmail.com>
2025-01-16 17:35:49 -0300

    Exception handler - findById

--- src/main/java/com/meiser/course/services/UserService.java
+++ src/main/java/com/meiser/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.meiser.course.entities.User;
 import com.meiser.course.repositories.UserRepository;
+import com.meiser.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
